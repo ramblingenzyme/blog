152630b749596f2a963217c2c9af3fc57259a474
Satvik Sharma <satvik.sharma2@gmail.com>
2016-07-12 18:30:01 +1000

    Restructure site to put blog posts in /blog instead

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 for file in $(ls md/); do
     echo $file
-    pandoc "./md/$file" -o "${file%%.*}.html" -c css/style.css -B include/navbar.html --title-prefix "Ramblings of an Enzyme"
+    case $file in
+        "about.md")
+            ;&
+        "archive.md")
+            ;&
+        "index.md")
+            output="./${file%%.*}.html"
+            ;;
+        *)
+            output="./blog/${file%%.*}.html"
+    esac
+    pandoc "./md/$file"     \
+    -o $output              \
+    -c css/style.css        \
+    -B include/navbar.html  \
+    --title-prefix "Ramblings of an Enzyme"
 done
--- md/index.md
+++ md/index.md
@@ -3,7 +3,7 @@ title: 'Home'
 ---
 
 ## Welcome to...something, I guess. ##
-I'm not really quite sure what to put on the front page... I guess you could go check out my almost non-existent [blog](./archive.md).
+I'm not really quite sure what to put on the front page... I guess you could go check out my almost non-existent [blog](./archive.html).
 
 ## Links ##
 * [Mirrexagon](http://www.mirrexagon.com/) - a friend's blog/site
