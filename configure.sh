3cf257ae16e675b94c467bccf9f70271271c3b67
Satvik Sharma <satvik.sharma2@gmail.com>
2020-10-28 09:38:59 +1100

    Swap over to ninja build

---
+++ configure.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+BUILD_FILE=build.ninja
+BUILD_DIR="build"
+COPIED_DIRS=( js images css public )
+COPIED_FILES=( CNAME keybase.txt )
+
+rm $BUILD_FILE 2>/dev/null || true
+
+write() {
+    echo "$@" | tee -a $BUILD_FILE
+}
+
+variable() {
+    write "$1 = $2"
+}
+
+rule() {
+    local name=$1
+    shift
+
+    write "rule $name"
+    write " description = $name \$in \$out"
+    write " command = $@"
+}
+
+build() {
+    local output=$1
+    shift
+
+    write "build $output: $@"
+}
+
+build_md() {
+    local file=""
+    local idir=$1
+    local odir="$BUILD_DIR/$2"
+
+    if [ -z "$2" ]; then
+        odir="$BUILD_DIR"
+    fi
+
+    for file in $(find "$idir" -maxdepth 1 -type f -name "*.md" -printf "%f\n"); do
+        build \
+            $odir/${file%%.*}.html \
+            md2html $idir/$file
+    done
+}
+
+rule \
+    md2html \
+    pandoc "\$in -o \$out --template \"include/template.html\" --title-prefix \"Ramblings of an Enzyme\""
+
+rule \
+    copy_dir \
+    cp "-r \$in \$out"
+
+rule \
+    copy \
+    cp "\$in \$out"
+
+build_md "src/articles" "blog"
+build_md "src"
+
+for DIR in ${COPIED_DIRS[@]}; do
+    build $BUILD_DIR/$DIR copy_dir src/$DIR
+done
+
+for FILE in ${COPIED_FILES[@]}; do
+    build $BUILD_DIR/$FILE copy src/$FILE
+done
+
+for FILE in $(find src -maxdepth 1 -name "*.html" -type f -printf "%f\n"); do
+    build $BUILD_DIR/$FILE copy src/$FILE
+done
+
